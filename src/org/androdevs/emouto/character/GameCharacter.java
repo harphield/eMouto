d8ab21e79f8f37575357848a856e58fc1e325649
harphield <harphield@gmail.com>
2011-06-20 14:06:19 +0200

    Arms done. Click the magical food button!

--- src/org/androdevs/emouto/character/GameCharacter.java
+++ src/org/androdevs/emouto/character/GameCharacter.java
@@ -91,13 +91,13 @@ public class GameCharacter extends Layer
 		AssetManager assetManager = game.getAssets();
 		bodyparts.clear();
 		try {
-			Random r = new Random();			
+			final Random r = new Random();			
 
 			String[] files = assetManager.list("gfx/character/hair_back");
 			String file = files[r.nextInt(files.length)];
 			
 			int[] pos = getBodypartMargins(file);
-			bodyparts.put(CHAR_BODYPART_HAIR_BACK, 		new BodyPart(pos[0], pos[1], new Sprite(0,0, TextureFactory.loadRegion("character/hair_back/"+file)), BodyPart.BP_TYPE_SINGLE));
+			bodyparts.put(CHAR_BODYPART_HAIR_BACK, 	new BodyPart(pos[0], pos[1], new Sprite(0,0, TextureFactory.loadRegion("character/hair_back/"+file)), BodyPart.BP_TYPE_SINGLE));
 			
 			files = assetManager.list("gfx/character/body");
 			file = files[r.nextInt(files.length)];
@@ -112,7 +112,6 @@ public class GameCharacter extends Layer
 
 			files = assetManager.list("gfx/character/mouth");
 			file = files[r.nextInt(files.length)];
-//			file = files[12];
 			pos = getBodypartMargins(file);
 			bodyparts.put(CHAR_BODYPART_MOUTH, 		new BodyPart(pos[0], pos[1], new Sprite(0,0, TextureFactory.loadRegion("character/mouth/"+file)), BodyPart.BP_TYPE_SINGLE));			
 			
@@ -125,6 +124,11 @@ public class GameCharacter extends Layer
 			file = files[r.nextInt(files.length)];
 			pos = getBodypartMargins(file);
 			bodyparts.put(CHAR_BODYPART_EYES, 		new BodyPart(pos[0], pos[1], new Sprite(0,0, TextureFactory.loadRegion("character/eyes/"+file)), BodyPart.BP_TYPE_SINGLE));
+
+			files = assetManager.list("gfx/character/arms");
+			file = files[r.nextInt(files.length)];
+			pos = getBodypartMargins(file);
+			bodyparts.put(CHAR_BODYPART_ARMS, 		new BodyPart(pos[0], pos[1], new Sprite(0,0, TextureFactory.loadRegion("character/arms/"+file)), BodyPart.BP_TYPE_SINGLE));
 			
 						
 		} catch (IOException e) {
--- src/org/androdevs/emouto/utility/TextureFactory.java
+++ src/org/androdevs/emouto/utility/TextureFactory.java
@@ -1,5 +1,7 @@
 package org.androdevs.emouto.utility;
 
+import java.util.Hashtable;
+
 import org.anddev.andengine.opengl.texture.Texture;
 import org.anddev.andengine.opengl.texture.TextureOptions;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
@@ -10,19 +12,36 @@ import org.androdevs.emouto.EmoutoGame;
 import org.androdevs.emouto.gui.GUI;
 
 
-public class TextureFactory {
+public class TextureFactory 
+{
 	public static String basePath = "gfx";
 	
+	/**
+	 * store loaded regions here.
+	 */
+	private static Hashtable<String, TextureRegion> loadedRegions;
+	
 	public static TextureRegion loadRegion(String file, int offsetX, int offsetY) {
+		
+		if (loadedRegions == null)
+			loadedRegions = new Hashtable<String, TextureRegion>();
+		
 		EmoutoGame game = GUI.game;
 		if (!file.contains(".")) file+=".png";
 		try {
+			
+			if (loadedRegions.containsKey(file))
+				return loadedRegions.get(file);
+			
 			AssetTextureSource source = new AssetTextureSource(GUI.game, basePath+"/"+file);  
 			Texture texture = new Texture(MathUtils.nextPowerOfTwo(source.getWidth()), 
 				MathUtils.nextPowerOfTwo(source.getHeight()), TextureOptions.NEAREST_PREMULTIPLYALPHA);
 
 			TextureRegion region = TextureRegionFactory.createFromAsset(texture, GUI.game, basePath+"/"+file, offsetX, offsetY);
 			game.loadTex(texture);
+			
+			loadedRegions.put(file, region);
+			
 			return region;
 		} catch(Exception e) {
 			e.printStackTrace();
